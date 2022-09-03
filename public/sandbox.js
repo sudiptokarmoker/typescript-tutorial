505bc1229538700d2a1c9f5ea1028f66303335e4
sudipto <sudiptocsi@gmail.com>
2022-09-03 19:56:05 +0600

    updated

--- public/sandbox.js
+++ public/sandbox.js
@@ -58,3 +58,63 @@ greet = () => {
 const add = (a, b) => {
     //console.log(a + b);
 };
+const logDetails = (uid, item) => {
+    console.log(`${item} has an uid ${uid}`);
+};
+const greetNew = (user) => {
+    console.log(`${user.name} says hello`);
+};
+// video no : 10
+// topics : Function Signatures
+// this is just function signature that means that have to follow this rules like this
+let greetFunction;
+greetFunction = (first, second) => {
+    console.log(first + ' ' + second);
+};
+greetFunction("hello", "world");
+// exmple 2 
+let calc;
+calc = (numberOne, numberTwo, action) => {
+    if (action === 'add') {
+        return numberOne + numberTwo;
+    }
+    else {
+        return numberOne - numberTwo;
+    }
+};
+// in the above example if we remove the else block it would show the error becuase if action value is not === add then it can show the error there
+let logDetailsNew;
+logDetailsNew = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
+};
+// video no: 11
+// topics: The DOM and Type Casting
+//const form = document.querySelector('.new-item-form')!;
+const form = document.querySelector('.new-item-form');
+// console.log(form.children);
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
+// Tutorial No: 12
+// TypeScript Tutorial #12 - Classe
+// topics: classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+}
+// now calling the Invoice method here
+const invOne = new Invoice('Mario', 'work on mario website', 250);
+// now print the value
+let invoices = [];
+invoices.push(invOne);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -75,6 +75,87 @@ greet = () => {
     console.log("hello i am here function");
 }
 
-const add = (a: number, b: number|string) => {
+const add = (a: number, b: number|string) : void => {
     //console.log(a + b);
 }
+
+//  videio no : 9
+// type alias
+type StringOrNumber = string | number;
+type objWithName = { name: string, uid: StringOrNumber }
+
+const logDetails = (uid: StringOrNumber, item: string) => {
+    console.log(`${item} has an uid ${uid}`);
+}
+
+const greetNew = (user: objWithName) => {
+    console.log(`${user.name} says hello`);
+}
+// video no : 10
+// topics : Function Signatures
+// this is just function signature that means that have to follow this rules like this
+let greetFunction: (a : string, b: string) => void;
+greetFunction = (first: string, second: string) => {
+    console.log(first + ' ' + second);
+}
+greetFunction("hello", "world");
+// exmple 2 
+let calc: (a: number, b: number, c: string) => number;
+calc = (numberOne: number, numberTwo: number, action: string) => {
+    if(action === 'add'){
+        return numberOne + numberTwo
+    } else {
+        return numberOne - numberTwo;
+    }
+}
+// in the above example if we remove the else block it would show the error becuase if action value is not === add then it can show the error there
+let logDetailsNew: (obj: {name: string, age: number}) => void;
+type person = {name: string, age: number};
+logDetailsNew = (ninja: person) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`)
+}
+// video no: 11
+// topics: The DOM and Type Casting
+//const form = document.querySelector('.new-item-form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.children);
+ const type = document.querySelector('#type') as HTMLSelectElement;
+ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+ const details = document.querySelector('#details') as HTMLInputElement;
+ const amount = document.querySelector('#amount') as HTMLInputElement;
+
+ form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    console.log(
+        type.value,
+        tofrom.value,
+        details.value,
+        amount.valueAsNumber
+    )
+ })
+
+ // Tutorial No: 12
+ // TypeScript Tutorial #12 - Classe
+// topics: classes
+
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format(){
+        return `${this.client} owes ${this.amount} for ${this.details}`
+    }
+}
+// now calling the Invoice method here
+const invOne = new Invoice('Mario', 'work on mario website', 250);
+// now print the value
+let invoices: Invoice[] = [];
+invoices.push(invOne);
